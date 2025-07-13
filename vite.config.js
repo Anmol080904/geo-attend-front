8e287c4660ff69dd989c0ffc82c40a8096c547dc
Anmol080904 <anmolchudasma1404@gmail.com>
2025-07-14 01:58:57 +0530

    update2.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // or vue, svelte, etc.
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 relative path
+  base: './'
 });
